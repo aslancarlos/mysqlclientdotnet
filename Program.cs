4e7776539adfcafb65b4cc31a300a9683dcaac5b
Ubuntu <ubuntu@ip-10-78-10-125.sa-east-1.compute.internal>
2024-02-07 17:30:04 +0000

    Simple exame without libs

---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
+using MySqlConnector;
+
+
+namespace NetPasswordSDK
+{
+  
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string SDK_FOLDER_PATH = "./dll/";
+
+            // Load the dll dynamically
+            var sdkDll = Assembly.LoadFrom(Path.Combine(SDK_FOLDER_PATH, "NetStandardPasswordSDK.dll"));
+            
+            // get the types from the dll
+            Type PasswordSDKType = sdkDll.GetType("CyberArk.AAM.NetStandardPasswordSDK.PasswordSDK");
+            Type PasswordRequestType = sdkDll.GetType("CyberArk.AAM.NetStandardPasswordSDK.PSDKPasswordRequest");
+            Type PasswordSDKExceptionType = sdkDll.GetType("CyberArk.AAM.NetStandardPasswordSDK.Exceptions.PSDKException");
+            Type PasswordResponseType = sdkDll.GetType("CyberArk.AAM.NetStandardPasswordSDK.PSDKPassword");
+
+
+            object passwordResponse = null;
+            
+
+            try
+            {
+                // Create Password Request
+                ConstructorInfo ctor = PasswordRequestType.GetConstructor(System.Type.EmptyTypes);
+                if (ctor == null)
+                {
+                    throw new Exception("Couldent create Password Request");
+                }
+
+                object passRequest;
+                passRequest = ctor.Invoke(null);
+
+                PropertyInfo propertyInfo = PasswordRequestType.GetProperty("ConnectionPort");
+                propertyInfo.SetValue(passRequest, 18923, null);
+                
+                propertyInfo = PasswordRequestType.GetProperty("ConnectionTimeout");
+                propertyInfo.SetValue(passRequest, 30, null);
+                
+                // Query propreties
+                propertyInfo = PasswordRequestType.GetProperty("AppID");
+                propertyInfo.SetValue(passRequest,"dotnet", null);
+                
+                // Was replaced by Query instead of passing the parameters, why do I need to place the object of type VirtualUserName
+                propertyInfo = PasswordRequestType.GetProperty("Query");
+                propertyInfo.SetValue(passRequest, "Safe=k8s-demo;Folder=root;VirtualUsername=demoaslan", null);
+
+            ////  Uncomment these if you want to use a default query, as UserName or other object type.
+                // propertyInfo = PasswordRequestType.GetProperty("Safe");
+                // propertyInfo.SetValue(passRequest, "k8s-demo", null);
+            
+                //    Not necessary    
+                // propertyInfo = PasswordRequestType.GetProperty("Folder");
+                // propertyInfo.SetValue(passRequest, "root", null);
+                
+                // propertyInfo = PasswordRequestType.GetProperty("Object");
+                // propertyInfo.SetValue(passRequest, "UserName=demoaslan", null);
+            ////
+                
+                propertyInfo = PasswordRequestType.GetProperty("Reason");
+                propertyInfo.SetValue(passRequest, "DotNet accessing", null);
+
+
+                // Sending the request to get the password
+                passwordResponse = PasswordSDKType.GetMethod("GetPassword").Invoke(null, new object[] { passRequest });
+
+
+                propertyInfo = PasswordResponseType.GetProperty("UserName");
+                var username = propertyInfo.GetValue(passwordResponse, null);
+                //Console.WriteLine(username);
+               
+                
+                propertyInfo = PasswordResponseType.GetProperty("Address");
+                var address = propertyInfo.GetValue(passwordResponse, null);
+                //use the address here
+                //Console.WriteLine(address);
+
+                // Analyzing the response
+                propertyInfo = PasswordResponseType.GetProperty("Content");
+                var password = (char[])propertyInfo.GetValue(passwordResponse, null);
+                //Console.WriteLine(password);
+                string passwd = new string(password);
+
+                // use the password here
+                 //using var connection = new MySqlConnection("Server={address};User ID={username};Password={password};Database=mydatabase");
+                var str = $"Server={address};User ID={username};Password={passwd}";
+                //Console.WriteLine(str);
+                          
+                using var connection = new MySqlConnection(str);
+                await connection.OpenAsync();
+
+                //using var command = new MySqlCommand("SELECT field FROM table;", connection);
+                using var command = new MySqlCommand("SHOW FULL PROCESSLIST;", connection);
+                using var reader = await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    var id = reader.GetValue(0);
+                    var user = reader.GetValue(1);
+                    var host = reader.GetValue(2);
+                    var db = reader.GetValue(3);
+                    var cmd = reader.GetValue(4);
+                    var time = reader.GetValue(5);
+                    var state = reader.GetValue(6);
+                    var info = reader.GetValue(7);
+                    // do something with 'value'
+                    //string top = "Id,User,Host,db,Command,Time,State,Info";
+                    //Console.WriteLine(top);
+                    string output_a = $"{id},{user},{host},{db},{cmd},{time},{state},{info}";
+                    Console.WriteLine(output_a);
+                }    
+    
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                // Always clear password form the memory
+                passwordResponse = null;
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# mysqlclientdotnet
-Small Example of MySQL client using CyberArk Credential Provider to fetch the credentials developer .NET
+
+# CyberArk Credential Provider - .NET Example on Ubuntu 20.04
+
+This repository contains a quick and practical example of a .NET application for recovering secrets via CyberArk Credential Provider (agent) and querying a MySQL database.
+
+Pre:
+
+  You must have the Credential Provider installed, with an Application ID with permissions in a safe environment.
+
+
+Remembering that the account must be real and accessible by the host it will run.
+
+
+# CyberArk Credential Provider - .NET Example
+
+This repository contains a quick and practical example of a .NET application for recovering secrets via CyberArk Credential Provider (agent) and querying a MySQL database.
+
+Pre:
+
+  You must have the Credential Provider installed, with an Application ID with permissions in a safe environment.
+
+
+Remembering that the account must be real and accessible by the host it will run.
+
+
+## Badges
+
+Add badges from somewhere like: [shields.io](https://shields.io/)
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+
+
+## Deployment
+
+To deploy this project run
+
+```bash
+  dotnet add package netstandardpasswordsdk
+  dotnet add package MySqlConnector 
+  dotnet run
+```
+
+
+## Acknowledgements
+
+ - [CyberArk Central Credential Provider - SDK .Net](https://docs.cyberark.com/credential-providers/latest/en/Content/CP%20and%20ASCP/Net-Application-Password-SDK.htm?tocpath=Developer%7CCredential%20Provider%7CApplication%20Password%20SDKs%7C.NET%20Application%20Password%20SDK%7C_____0)
+ - [Install the .NET SDK or the .NET Runtime on Ubuntu](https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu)
+
+
